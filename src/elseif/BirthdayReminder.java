1368d29dab9161187123f3686986a74a94e3db86
kcshyffer <league@WTS14-2.attlocal.net>
2018-10-10 18:27:32 -0700

    Birthday, ShinyObjects

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,36 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "March 26th";
+		String dadsBirthday = "May 8th";
+		String myBirthday = "May 24th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String s = JOptionPane.showInputDialog("Who's birthday do you want to know?");
 		// 3. Print out what the user typed
-		
+		System.out.println(s);
 		// 4. if user asked for "mom"
 			//print mom's birthday
+		if (s .equals ("mom")) {
+			System.out.println(momsBirthday);
+			
+		}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+		else if (s .equals ("dad")) {
+			System.out.println(dadsBirthday);
+			
+		}
 		// 6. if user asked for your name
 			// print myBirthday
+		else if (s .equals ("kyle")) {
+			System.out.println(myBirthday);
+			
+		}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else {
+			System.out.println("Sorry Chief, that aint it.");
+			
+		}
 	} 
 }
--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -8,18 +8,22 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+		String s = JOptionPane.showInputDialog("How many shiny objects do you want?");
+		int answer = Integer.parseInt(s);
 		// 3. Play the sound that many times
-
+		for (int i = 0; i < answer; i++) {
+			
+		
 		// 1. Call the method below
-
+			playMisterZee();
 	}
-
+}
 	public static void playMisterZee() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(ShinyObjects.class.getResource("shiny-objects.wav"));
